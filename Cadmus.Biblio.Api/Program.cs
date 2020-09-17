e9014d98f4a297be910d6304009036d245cd2684
Myrmex <dfusi@hotmail.com>
2020-09-17 20:10:51 +0200

    More infrastructure

--- Cadmus.Biblio.Api/Program.cs
+++ Cadmus.Biblio.Api/Program.cs
@@ -1,26 +1,86 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Cadmus.Api.Services.Seeding;
+using System.Threading.Tasks;
 
 namespace Cadmus.Biblio.Api
 {
-    public class Program
+    public sealed class Program
     {
-        public static void Main(string[] args)
+        private static void DumpEnvironmentVars()
         {
-            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("ENVIRONMENT VARIABLES:");
+            IDictionary dct = Environment.GetEnvironmentVariables();
+            List<string> keys = new List<string>();
+            var enumerator = dct.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                keys.Add(((DictionaryEntry)enumerator.Current).Key.ToString());
+            }
+
+            foreach (string key in keys.OrderBy(s => s))
+                Console.WriteLine($"{key} = {dct[key]}");
         }
 
+        /// <summary>
+        /// Creates the host builder.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
+
+        public static async Task<int> Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+#if DEBUG
+                .WriteTo.File("cadmus-biblio-log.txt", rollingInterval: RollingInterval.Day)
+#endif
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host");
+                DumpEnvironmentVars();
+
+                // this is the place for seeding:
+                // see https://stackoverflow.com/questions/45148389/how-to-seed-in-entity-framework-core-2
+                // and https://docs.microsoft.com/en-us/aspnet/core/migration/1x-to-2x/?view=aspnetcore-2.1#move-database-initialization-code
+                var host = await CreateHostBuilder(args)
+                    .UseSerilog()
+                    .Build()
+                    .SeedAsync(accounts: true, data: false); // see Services/HostSeedExtension
+
+                host.Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                Debug.WriteLine(ex.ToString());
+                Console.WriteLine(ex.ToString());
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
     }
 }
